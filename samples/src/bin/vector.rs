42cc139c36b628477737d6750b697b0c188c7dcd
Valentin Kofman <kvmisis@gmail.com>
2022-09-18 20:39:17 +0200

    Make Vector array based, add mut iterator, rename to SmallVec.

--- samples/src/bin/vector.rs
+++ samples/src/bin/vector.rs
@@ -1,39 +1,56 @@
-struct Vector<T> {
-    data: Vec<T>,
+struct SmallVec<T: Default, const N: usize> {
+    data: [T; N],
+    size: usize,
 }
 
-struct VectorIterator<'a, T> {
-    vec: &'a Vector<T>,
-    pos: usize,
-}
-
-impl<T> Vector<T> {
+impl<T: Copy + Default, const N: usize> SmallVec<T, N> {
     fn new() -> Self {
-        Self { data: Vec::new() }
+        Self {
+            data: [Default::default(); N],
+            size: 0,
+        }
+    }
+
+    fn iter(&self) -> std::slice::Iter<T> {
+        self.data[..self.size].iter()
+    }
+
+    fn iter_mut(&mut self) -> std::slice::IterMut<T> {
+        self.data[..self.size].iter_mut()
+    }
+
+    fn push(&mut self, elem: T) {
+        if self.size >= N {
+            panic!("Array too small");
+        }
+        self.data[self.size] = elem;
+        self.size += 1;
     }
 }
 
-impl<T: PartialEq> PartialEq for Vector<T> {
+impl<T: Default + PartialEq, const N: usize> PartialEq for SmallVec<T, N> {
     fn eq(&self, other: &Self) -> bool {
         self.data.eq(&other.data)
     }
 }
 
-impl<T: Eq> Eq for Vector<T> {}
+impl<T: Default + Eq, const N: usize> Eq for SmallVec<T, N> {}
 
-impl<T> Default for Vector<T> {
+impl<T: Default + Copy, const N: usize> Default for SmallVec<T, N> {
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl<T> Extend<T> for Vector<T> {
+impl<T: Default + Copy, const N: usize> Extend<T> for SmallVec<T, N> {
     fn extend<I: IntoIterator<Item = T>>(&mut self, iter: I) {
-        self.data.extend(iter);
+        for x in iter {
+            self.push(x);
+        }
     }
 }
 
-impl<T> FromIterator<T> for Vector<T> {
+impl<T: Default + Copy, const N: usize> FromIterator<T> for SmallVec<T, N> {
     fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
         let mut res: Self = Default::default();
         res.extend(iter);
@@ -41,37 +58,42 @@ impl<T> FromIterator<T> for Vector<T> {
     }
 }
 
-impl<'a, T> VectorIterator<'a, T> {
-    fn new(vec: &'a Vector<T>) -> VectorIterator<'a, T> {
-        VectorIterator { vec, pos: 0 }
+impl<T: Default, const N: usize> AsRef<[T]> for SmallVec<T, N> {
+    fn as_ref(&self) -> &[T] {
+        &self.data[..self.size]
     }
 }
 
-impl<'a, T> Iterator for VectorIterator<'a, T> {
-    type Item = &'a T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.pos < self.vec.data.len() {
-            self.pos += 1;
-            Some(&self.vec.data[self.pos - 1])
-        } else {
-            None
-        }
+impl<T: Default, const N: usize> AsMut<[T]> for SmallVec<T, N> {
+    fn as_mut(&mut self) -> &mut [T] {
+        &mut self.data[..self.size]
     }
 }
 
-impl<'a, T> IntoIterator for &'a Vector<T> {
+impl<'a, T: Default + Copy, const N: usize> IntoIterator for &'a SmallVec<T, N> {
     type Item = &'a T;
-    type IntoIter = VectorIterator<'a, T>;
+    type IntoIter = std::slice::Iter<'a, T>;
 
     fn into_iter(self) -> Self::IntoIter {
-        Self::IntoIter::new(self)
+        self.iter()
+    }
+}
+
+impl<'a, T: Default + Copy, const N: usize> IntoIterator for &'a mut SmallVec<T, N> {
+    type Item = &'a mut T;
+    type IntoIter = std::slice::IterMut<'a, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter_mut()
     }
 }
 
 fn main() {
-    let items: Vector<i32> = (0..10).collect();
+    let mut items: SmallVec<i32, 16> = (0..10).collect();
+    for x in &mut items {
+        *x *= 2;
+    }
     for x in &items {
-        println!("item: {}", x);
+        println!("item: {}", *x);
     }
 }
